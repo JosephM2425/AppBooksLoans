222a968c58fc1568df6a75bb1bc4b9d50246cb5b
Joseph Murillo <josephmurillo2425@gmail.com>
2021-04-28 16:24:32 -0600

    ADD verfication for loans user

--- app/controllers/my_loans_controller.rb
+++ app/controllers/my_loans_controller.rb
@@ -1,8 +1,16 @@
 class MyLoansController < ApplicationController
+  before_action :set_loan, only: %i[ show ]
   def index
     @my_loans = current_user.loans.current_books
   end
 
   def show
   end
+
+  private
+
+  def set_loan
+    @loan = Loan.find(params[:id])
+  end
+
 end
