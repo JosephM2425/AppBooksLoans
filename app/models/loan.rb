7fc50abdf6f1139f31b156ff85131aaa92b727f1
Joseph Murillo <josephmurillo2425@gmail.com>
2021-04-29 11:05:42 -0600

    ADD testing for pdf preview

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -2,6 +2,6 @@ class Loan < ApplicationRecord
   belongs_to :user
   belongs_to :book
   
-  scope :current_books, ->() {where('start_date <= ? AND end_date > ?', Time.now.beginning_of_day, Time.now.end_of_day)}
+  scope :current_books, ->() {where('start_date <= ? AND end_date >= ?', Time.now.beginning_of_day, Time.now.end_of_day)}
 
 end
