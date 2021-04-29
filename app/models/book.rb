4c7e7eff507ed98d8ed3e23f6b9117054894beb5
Joseph Murillo <josephmurillo2425@gmail.com>
2021-04-29 17:08:50 -0600

    ADD function for path file of active storage

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,16 @@ class Book < ApplicationRecord
 
     has_one_attached :pdf_book
     has_one_attached :cover
+
+
+    # Sacar el archivo y guardarlo en la carpeta tmp, para obtener el path del archivo
+    def process_attachment
+        #process_attachment 
+        pdf_attachment_path = "#{Dir.tmpdir}/#{self.pdf_book.filename}" 
+        File.open(pdf_attachment_path, 'wb') do |file| 
+            file.write(self.pdf_book.download) 
+        end
+        return pdf_attachment_path 
+    end 
+
 end
