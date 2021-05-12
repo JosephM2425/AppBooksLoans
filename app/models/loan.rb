3f55f367bd9d16fce3299123a395453d1f571684
Joseph Murillo <josephmurillo2425@gmail.com>
2021-05-12 10:18:42 -0600

    disable mail send to deploy heroku

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,7 +1,7 @@
 class Loan < ApplicationRecord
   belongs_to :user
   belongs_to :book
-  after_create :notification_loan_send
+  #after_create :notification_loan_send
   
   scope :current_loans, ->() {where('start_date <= ? AND end_date >= ? AND active = true', Time.now.beginning_of_day, Time.now.end_of_day)}
   
